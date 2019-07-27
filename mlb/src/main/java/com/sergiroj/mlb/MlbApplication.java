fc0cf392a4183a7d60a0d2e4b3979872c81cffbc
ser <sergiorojasvega@hotmail.com>
2019-07-27 16:02:56 -0500

    Initial commit

---
+++ mlb/src/main/java/com/sergiroj/mlb/MlbApplication.java
@@ -0,0 +1,13 @@
+package com.sergiroj.mlb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbApplication.class, args);
+	}
+
+}
